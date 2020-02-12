4b140f97b889fbb21c206c837032a46dac1f179c
amitsb18 <iambalbatti@gmail.com>
2020-02-12 11:32:13 +0530

    Added dependency for drivers, drivers are dynamic

--- Assignment2/src/generic/BrowserFunction.java
+++ Assignment2/src/generic/BrowserFunction.java
@@ -2,18 +2,23 @@ package generic;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserFunction {
 
 	static {
-		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		// System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriverManager.chromedriver().version("80.0.3987.16").setup();
 	}
 
 	public static WebDriver driver;
 
 	// Open chrome browser and enter url
 	public void openBrowser(String url) {
-		driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 		driver.get(url);
 	}
